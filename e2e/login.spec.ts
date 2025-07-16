0a1ac4923b1c43272000343be039da4051a2e295
Fernando Papito <papitodev@proton.me>
2025-07-16 14:11:02 -0300

    page objets

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,19 +1,59 @@
 import { test, expect } from '@playwright/test'
 
+import { getLoginPage } from '../support/pages/LoginPage'
+
 test('deve logar com sucesso', async ({ page }) => {
 
+    const loginPage = getLoginPage(page)
+
     const user = {
+        name: 'Fernando',
         username: 'papito',
         password: 'pwd123'
     }
 
-    await page.goto('http://localhost:3000/login')
+    await loginPage.open()
+    await loginPage.submit(user.username, user.password)
 
-    await page.locator('input[placeholder="Seu @username incrível"]').fill(user.username)
-    await page.locator('input[placeholder="Digite sua senha secreta"]').fill(user.password)
+    await expect(page.locator('h1')).toContainText(`Olá, ${user.name}! 👋`)
+});
 
-    await page.locator('button[type="submit"]').click()
+test('não deve logar com senha incorreta', async ({ page }) => {
 
-    const title = page.locator('h1')
-    await expect(title).toContainText('Olá, Fernando!')
+    const loginPage = getLoginPage(page)
+
+
+    const user = {
+        name: 'Fernando',
+        username: 'papito',
+        password: '123456'
+    }
+
+    await loginPage.open()
+    await loginPage.submit(user.username, user.password)
+
+    const toast = page.locator('.toast')
+
+    await expect(toast).toContainText('Oops!')
+    await expect(toast).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais e tente novamente.');
+});
+
+test('não deve logar com usuário não cadastrado', async ({ page }) => {
+
+    const loginPage = getLoginPage(page)
+
+
+    const user = {
+        name: 'Fernando',
+        username: 'not-found',
+        password: '123456'
+    }
+
+    await loginPage.open()
+    await loginPage.submit(user.username, user.password)
+
+    const toast = page.locator('.toast')
+
+    await expect(toast).toContainText('Oops!')
+    await expect(toast).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais e tente novamente.');
 });
