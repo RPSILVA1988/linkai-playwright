9e148655bb0e13eec0df11ae945c806f17d27ce5
Fernando Papito <papitodev@proton.me>
2025-07-16 22:57:10 -0300

    Components

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test'
 
 import { getLoginPage } from '../support/pages/LoginPage'
+import { getDashPage } from '../support/pages/DashPage'
+import { getToast } from '../support/pages/components/Toast'
 
 test('deve logar com sucesso', async ({ page }) => {
 
     const loginPage = getLoginPage(page)
+    const dashPage = getDashPage(page)
+    const toast = getToast(page)
 
     const user = {
         name: 'Fernando',
@@ -15,13 +19,15 @@ test('deve logar com sucesso', async ({ page }) => {
     await loginPage.open()
     await loginPage.submit(user.username, user.password)
 
-    await expect(page.locator('h1')).toContainText(`Olá, ${user.name}! 👋`)
-});
+    await expect(dashPage.welcome()).toContainText(`Olá, ${user.name}! 👋`)
+    await expect(toast.element()).toContainText('Login realizado com sucesso!')
+    await expect(toast.element()).toContainText('Bem-vindo de volta ao Linkaí.')
+})
 
 test('não deve logar com senha incorreta', async ({ page }) => {
 
     const loginPage = getLoginPage(page)
-
+    const toast = getToast(page)
 
     const user = {
         name: 'Fernando',
@@ -32,16 +38,14 @@ test('não deve logar com senha incorreta', async ({ page }) => {
     await loginPage.open()
     await loginPage.submit(user.username, user.password)
 
-    const toast = page.locator('.toast')
-
-    await expect(toast).toContainText('Oops!')
-    await expect(toast).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais e tente novamente.');
-});
+    await expect(toast.element()).toContainText('Oops!')
+    await expect(toast.element()).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais e tente novamente.')
+})
 
 test('não deve logar com usuário não cadastrado', async ({ page }) => {
 
     const loginPage = getLoginPage(page)
-
+    const toast = getToast(page)
 
     const user = {
         name: 'Fernando',
@@ -52,8 +56,60 @@ test('não deve logar com usuário não cadastrado', async ({ page }) => {
     await loginPage.open()
     await loginPage.submit(user.username, user.password)
 
-    const toast = page.locator('.toast')
+    await expect(toast.element()).toContainText('Oops!')
+    await expect(toast.element()).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais e tente novamente.')
+})
 
-    await expect(toast).toContainText('Oops!')
-    await expect(toast).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais e tente novamente.');
-});
+test('não deve logar quando não informo nenhum dos campos', async ({ page }) => {
+
+    const loginPage = getLoginPage(page)
+    const toast = getToast(page)
+
+    const user = {
+        name: 'Fernando',
+        username: '',
+        password: ''
+    }
+
+    await loginPage.open()
+    await loginPage.submit(user.username, user.password)
+
+    await expect(toast.element()).toContainText('Campos obrigatórios')
+    await expect(toast.element()).toContainText('Por favor, preencha todos os campos.')
+})
+
+test('não deve logar quando não informo o usuário', async ({ page }) => {
+
+    const loginPage = getLoginPage(page)
+    const toast = getToast(page)
+
+    const user = {
+        name: 'Fernando',
+        username: '',
+        password: 'pwd123'
+    }
+
+    await loginPage.open()
+    await loginPage.submit(user.username, user.password)
+
+    await expect(toast.element()).toContainText('Campos obrigatórios')
+    await expect(toast.element()).toContainText('Por favor, preencha todos os campos.')
+})
+
+test('não deve logar quando não informo a senha', async ({ page }) => {
+
+    const loginPage = getLoginPage(page)
+    const toast = getToast(page)
+
+    const user = {
+        name: 'Fernando',
+        username: 'papito',
+        password: ''
+    }
+
+    await loginPage.open()
+    await loginPage.submit(user.username, user.password)
+
+    await expect(toast.element()).toContainText('Campos obrigatórios')
+    await expect(toast.element()).toContainText('Por favor, preencha todos os campos.')
+})
